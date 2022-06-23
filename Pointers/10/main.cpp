be44d3c69e11e8e3c61d4555c288099605877d04
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-06-23 11:27:17 +0530

    Done

--- Pointers/10/main.cpp
+++ Pointers/10/main.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
 using namespace std;
 
-void fun(int a[]) {
+void fun(int a[])
+{
     cout << a[0] << " ";
 }
 
-int main() {
-    int a[] = {1, 2, 3, 4};
-    fun(a + 1);
-    cout << a[0];
+int main()
+{
+    int w[] = {1, 2, 3, 4};
+    fun(w + 1);
+    cout << w[0] << endl;
+
+    // int a = 10;
+    // int *p = &a;
+    // int **q = &p;
+    // int b = 20;
+    // *q = &b;
+    // (*p)++;
+    // cout << a << " " << b << endl;
+
+    // int z = 100;
+    // int *x = &z;
+    // int **n = &x;
+    // int m = (**n)++ + 4;
+    // cout << z << " " << m << endl;
+
+    int a = 100;
+    int *p = &a;
+    int **q = &p;
+    int b = (**q)++;
+    int *r = *q;
+    (*r)++;
+    cout << a << " " << b << endl;
 }
 
 // void square(int *p){
