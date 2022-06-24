ca7591ec6a17e6bbc1ace802e655fd0765b254ef
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-06-24 10:22:38 +0530

    Done

---
+++ Recursion/3/main.cpp
@@ -0,0 +1,36 @@
+// #include <iostream>
+// using namespace std;
+
+// int func(int num){
+//     return func(num- 1);
+// }
+
+// int main() {
+//     int num = 5;
+//     int ans = func(num - 1);
+//     cout << ans;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// void print(int n)
+// {
+//   if (n < 0)
+//   {
+//     return;
+//   }
+//   if (n == 0)
+//   {
+//     cout << n << " ";
+//     return;
+//   }
+//   print(n--);
+//   cout << n << " ";
+// }
+
+// int main()
+// {
+//   int num = 3;
+//   print(num);
+// }
