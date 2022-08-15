25093bdb68c8694d05e4c2b4df92d88a57e3aa55
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-08-15 11:11:14 +0530

    Adds queue code

---
+++ Queues/0) Normal Queue/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+class QueueUsingArray
+{
+  int *data;
+  int front;
+  int rear;
+  int capacity;
+
+public:
+  QueueUsingArray(int size)
+  {
+    data = new int[size];
+    front = -1;
+    rear = -1;
+    capacity = size;
+  }
+
+  void enqueue(int element)
+  {
+    if (rear == capacity)
+    {
+      cout << "Queue Full" << endl;
+    }
+    if (front == -1)
+    {
+      front++;
+      data[front] = element;
+      rear++;
+    }
+    else
+    {
+      rear++;
+      data[rear] = element;
+    }
+  }
+
+  int dequeue()
+  {
+    if (front == -1)
+    {
+      cout << "Queue is empty" << endl;
+      return -1;
+    }
+    else
+    {
+      return data[front];
+      front++;
+    }
+  }
+
+  int top()
+  {
+    if (front == -1)
+    {
+      cout << "Queue is empty" << endl;
+      return -1;
+    }
+    else
+    {
+      return data[front];
+    }
+  }
+};
+
+int main()
+{
+  QueueUsingArray s(4);
+  s.enqueue(10);
+  s.enqueue(20);
+  s.enqueue(30);
+  s.enqueue(40);
+  s.enqueue(50);
+
+  cout << s.dequeue() << endl;
+  cout << s.dequeue() << endl;
+  cout << s.dequeue() << endl;
+  cout << s.top() << endl;
+}
